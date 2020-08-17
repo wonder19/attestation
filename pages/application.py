0b9f651d137a7a0f2d88c03ad1a0e66dca08e9a0
Mari Dorokhova <m.dorokhova@pnlfin.tech>
2020-08-17 23:07:37 +0500

    fix ci fix logs

--- pages/application.py
+++ pages/application.py
@@ -4,8 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-from common.logging import setup
-from common.utils import logging as log
 from pages.deposit import DepositPage
 from pages.login import LoginPage
 from pages.main import MainPage
@@ -15,8 +13,6 @@ logger = logging.getLogger()
 
 class Application:
     def __init__(self, base_url, headless):
-        setup("INFO")
-        logger.setLevel("INFO")
         driver_path = ChromeDriverManager().install()
         options: Options = Options()
         options.headless = headless
@@ -26,8 +22,8 @@ class Application:
         self.main_page = MainPage(self)
         self.deposit_page = DepositPage(self)
 
-    # @log("Open main page")
     def open_main_page(self):
+        logger.info('Open main page')
         self.wd.get(self.base_url + "welcome")
 
     def open_deposit_condition_confirm_pae(self, value:str):
